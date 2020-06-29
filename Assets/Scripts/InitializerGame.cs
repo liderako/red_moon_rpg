f88b5e35af83a907292b5c26f3b671030440c0c8
liderako <trotillostudio@gmail.com>
2020-06-29 08:35:10 +0300

    add new scene for initializer basic system

---
+++ Assets/Scripts/InitializerGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializerGame : MonoBehaviour
+{
+    public void Start()
+    {
+        StartCoroutine(wait());
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSeconds(0.01f);
+        Contexts.sharedInstance.game.GetEntityWithName("Level").AddNextLevelName("Map");
+    }
+}
