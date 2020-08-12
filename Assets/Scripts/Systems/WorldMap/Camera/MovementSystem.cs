475fe57d6c4201a40ab388323f9fd1eeb71e7b33
Antonio Sviridov <ant.svirido@gmail.com>
2020-08-12 19:44:41 +0200

    исправление камеры

--- Assets/Scripts/Systems/WorldMap/Camera/MovementSystem.cs
+++ Assets/Scripts/Systems/WorldMap/Camera/MovementSystem.cs
@@ -23,27 +23,35 @@ namespace RedMoonRPG.Systems.WorldMap.Camera
         {
             GameEntity camera = _contexts.game.GetEntityWithName(Tags.camera);
             GameEntity level = _contexts.game.GetEntityWithName(Tags.level);
-            Vector3 pos = camera.transform.value.position;
+            Vector3 pos = camera.transform.value.localPosition;
             float speed = (Input.GetKey("left shift") ? camera.forceSpeed.value : camera.speed.value);
             if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - camera.borderThickness.value)
             {
-                //pos.z -= speed * Time.deltaTime;
-                pos += Vector3.forward * speed * Time.deltaTime;
+                float y = pos.y;
+                pos += camera.transform.value.forward.normalized * speed * Time.deltaTime;
+                pos.y = y;
                 camera.isFreeCamera = true;
             }
             if (Input.GetKey("s") || Input.mousePosition.y <= camera.borderThickness.value)
             {
-                pos += Vector3.back * speed * Time.deltaTime;
+                float y = pos.y;
+                pos -= camera.transform.value.forward * speed * Time.deltaTime;
+                pos.y = y;
                 camera.isFreeCamera = true;
             }
             if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - camera.borderThickness.value)
             {
-                pos += Vector3.right * speed * Time.deltaTime;
+                float y = pos.y;
+                pos += camera.transform.value.right * speed * Time.deltaTime;
+                pos.y = y;
                 camera.isFreeCamera = true;
             }
             if (Input.GetKey("a") || Input.mousePosition.x <= camera.borderThickness.value)
             {
-                pos += Vector3.left * speed * Time.deltaTime;
+                float y = pos.y;
+                pos -= camera.transform.value.right * speed * Time.deltaTime;
+                pos.y = y;
+
                 camera.isFreeCamera = true;
             }
             if (Input.GetAxis("Mouse ScrollWheel") != 0)
@@ -52,11 +60,11 @@ namespace RedMoonRPG.Systems.WorldMap.Camera
                 pos.y += (camera.forceSpeed.value * t) * Time.deltaTime;
                 pos.y = Mathf.Clamp(pos.y, level.limitMap.axisY.x, level.limitMap.axisY.y);
             }
-            if (pos.x != camera.transform.value.position.x || pos.z != camera.transform.value.position.z || pos.y != camera.transform.value.position.y)
+            if (pos.x != camera.transform.value.localPosition.x || pos.z != camera.transform.value.localPosition.z || pos.y != camera.transform.value.localPosition.y)
             {
                 pos.x = Mathf.Clamp(pos.x, level.limitMap.axisX.x, level.limitMap.axisX.y);
                 pos.z = Mathf.Clamp(pos.z, level.limitMap.axisZ.x, level.limitMap.axisZ.y);
-                camera.transform.value.position = pos;
+                camera.transform.value.localPosition = pos;
             }
             if (Input.GetKeyDown(KeyCode.Tab))
             {
